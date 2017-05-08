659bad6697736920282cee41f805475ff41efb20
Peter Moore <peter.moore@pmoordc01.local>
2017-05-08 09:06:53 -0400

    Validation script for flagging values significantly different from previous values or percentages above 100

--- helpers/Connection.php
+++ helpers/Connection.php
@@ -1,9 +1,9 @@
 <?php
 // Server Settings
-$serverName="PETERMOORE-PC\SQLEXPRESS";
-$user='sa';
-$password='Glenlake!070288';
-$database="fulton_county";
+$serverName="";
+$user='';
+$password='';
+$database="";
 $conn = odbc_connect("Driver={SQL Server Native Client 10.0};Server=$serverName;Database=$database;", $user, $password);
 if(!$conn) {
   die(odbc_errormsg());
