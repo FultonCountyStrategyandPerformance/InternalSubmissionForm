76295f36250e3dd1644c27354cb709f9f7cb15bc
Peter Moore <peter.moore@pmoordc01.local>
2017-05-10 15:21:54 -0400

    default KPI table

--- pages/defaultKPIS_ProgramBreakdown.php
+++ pages/defaultKPIS_ProgramBreakdown.php
@@ -52,7 +52,6 @@
                   $grid .= "<tr><td style='width:100%' class='tooltip'>".$row['MeasureName']."<span class='tooltiptext'>".$row['Description']."</span></td><td class='validation' style='width:15%'>".
                   "<input id='".round($row['MeasureID'])."' name='kpi_values[".round($row['MeasureID'])."]' type='number' step='any' value='".$v['Value']."' required/><span class='validtext'>".$count_warning_text."</span></td><td style='width:10%' id='unit'>".$row["MeasureUnit"]."</td></tr>";
                 }
-              }
           }
           $grid .= "</table>";
           echo $grid;
