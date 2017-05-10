c8ba5fff897e6462739a92be1c3032f5754af390
Peter Moore <peter.moore@pmoordc01.local>
2017-05-10 14:07:21 -0400

    fix login validation

--- helpers/loginValidation.php
+++ helpers/loginValidation.php
@@ -3,7 +3,11 @@ function validate($conn, $user, $password, $users_table) {
   $user_query = "SELECT *
     FROM ".$users_table."
     WHERE user_name like '".$user."' AND user_password = '".$password."'";
+  echo "<script>console.log('".$user_query."')</script>";
   $user_result = sqlsrv_query($conn, $user_query);
+  if(!$user_result) {
+    echo sqlsrv_errors();
+  }
   if(sqlsrv_num_rows($user_result) == 0) {
     return array (false, $user,"", "NONE", 0);
   }
